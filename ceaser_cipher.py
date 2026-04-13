e85482391e3c8638192a48cd89ca7d99468fb671
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:23:48 +0530

    Add files via upload

---
+++ ceaser_cipher.py
@@ -0,0 +1,14 @@
+'''encrypt a word using ceaser cipher'''
+'''word=input("enter a word :")
+key=int(input("enter a key :"))
+result=""
+for ch in word:
+    if ch.isalpha():
+        if ch.islower():
+            new=chr(ord(ch)+key)
+            result+=new
+        else:
+            new=chr(ord(ch)+key)
+            result+=new
+print(result)'''
+
