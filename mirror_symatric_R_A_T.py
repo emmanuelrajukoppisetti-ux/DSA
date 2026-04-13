05636d33bf5cbb97283329727e01801d5f45cf6b
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:36:53 +0530

    Add files via upload

---
+++ mirror_symatric_R_A_T.py
@@ -0,0 +1,10 @@
+#mirror symetric right angle triangle
+n=int(input("enter the value of n"))
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==n-1 or i==j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    
+    print()
