ffa9dacaa569a83bfd047b33bd9bf4fe9f221fb9
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:31:13 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,16 @@
+#wrire a code to print all even n natural numbers
+'''n=int(input('enter the value of n:'))
+i=1
+while i<=n:
+    if i%2==0:
+        print(i,end=' ')
+    i+=1'''
+
+
+#check Automarphic number
+'''n=int(input("enter a number"))
+square=n*n
+if str(square).endswith(str(n)):
+    print("Automarphic number")
+else:
+    print("not Automarphic number")'''
