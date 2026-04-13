bcaec7c54cac42e5497a1868f9d9f0f926107b10
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:32:23 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,40 @@
+#no arg no return type
+'''def hi():
+    print("hellow")
+    print("students",end=" ")
+    print("good morning")
+hi()'''
+
+#arg pass no return type
+'''def summate(a,b):
+    sum=a+b
+    print("total",sum)
+def diff(a,b):
+    d=a-b
+    print("difference",d)
+def multiply(a,b):
+    mul=a*b
+    print("multiply",mul)
+x=int(input("enter a number"))
+y=int(input("enter b value"))
+summate(x,y)
+diff(x,y)
+multiply(x,y)'''
+
+#no arg return value
+'''def addition():
+    a=int(input("enter a number"))
+    b=int(input("enter b number"))
+    c=a+b
+    return c
+summate=addition()
+print("sum:",summate)'''
+
+# arg pass return value
+
+'''def expo(a,b):
+    return a**b
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+result=expo(x,y)
+print("result",result)'''
