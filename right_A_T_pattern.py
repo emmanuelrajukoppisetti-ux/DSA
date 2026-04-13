234c7b4568f01218055d06b27e987e4d748e9f8c
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:40:51 +0530

    Add files via upload

---
+++ right_A_T_pattern.py
@@ -0,0 +1,10 @@
+#right angle triangle
+n=int(input("enter the value of : "))
+for i in range(n):
+    for j in range(n):
+        if j==0 or i==j or i==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    
+    print()
