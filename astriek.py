89815e2774078f0e8aba0daff724a72eadda630e
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:20:21 +0530

    Add files via upload

---
+++ astriek.py
@@ -0,0 +1,9 @@
+n=int(input("enter the value of n"))
+for i in range(n):
+    for j in range(n):
+        if i==n//2 or j==n//2 or i==j or (i+j)==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    
+    print()
