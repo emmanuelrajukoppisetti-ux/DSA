1c9d912f5a21fc9362d5fe9671b6c8e0bba9542b
emmanuelrajukoppisetti-ux <emmanuelrajukoppisetti@gmail.com>
2026-04-13 19:18:28 +0530

    Add files via upload

---
+++ arn_strong_number.py
@@ -0,0 +1,13 @@
+#check arm strong number or not
+'''num=int(input("enter a number"))
+copy=num
+s=0
+while num!=0:
+    r=num%10
+    s+=r*r*r
+    num//=10
+if copy==s:
+    print(copy,"is an arm strong number)
+else:
+    print(copy,"is not an arm strong number")'''
+
